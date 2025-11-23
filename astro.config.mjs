96d1858d1c9fb044510037a09f7dcad8fd2323e3
Martín Giner <martinginernavarro7@gmail.com>
2025-11-23 11:03:55 +0100

    feat: add base layout, configure robots.txt, and update project setup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   adapter: node({ mode: 'standalone' }),
   site: 'https://martinginer9.github.io',
 });
